932d884ae40e41e99e05103dee1270e4409b5f0f
David Jenner <353397+davidjenner@users.noreply.github.com>
2025-03-11 19:29:27 +0000

    Update Welcome.tsx
    updated welcome for ADHD etc

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,10 +1,8 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am David</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+    Welcome to your neurodivergent-friendly corner of the internet! This is a safe and supportive space for folks with autism, ADHD, dyslexia, and more, where you can find helpful ideas and strategies tailored to your unique brain.
     </p>
     <p>
       <a className="underline" href="https://github.com/ascorbic/daneel#readme">
