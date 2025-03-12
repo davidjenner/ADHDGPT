a00eaf5140fd9cf284c50e35a75636bc0bfb556f
David Jenner <353397+davidjenner@users.noreply.github.com>
2025-03-12 19:36:08 +0000

    Update Welcome.tsx
    added green

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,5 +1,7 @@
+import React from 'react';
+
 export const Welcome: React.FC = () => (
-  <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
+  <div className="bg-green-50 border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am David</h1>
     <p>
     Welcome to your neurodivergent-friendly corner of the internet! This is a safe and supportive space for folks with autism, ADHD, dyslexia, and more, where you can find helpful ideas and strategies tailored to your unique brain.
@@ -11,3 +13,5 @@ export const Welcome: React.FC = () => (
     </p>
   </div>
 );
+
+export default Welcome;
